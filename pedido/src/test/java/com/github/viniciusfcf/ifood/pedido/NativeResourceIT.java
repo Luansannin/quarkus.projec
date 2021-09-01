f394ea51a1de8d6026a4263c093f044104e5f6bd
Luansannin <88343977+Luansannin@users.noreply.github.com>
2021-08-31 21:16:23 -0300

    first commit

---
+++ pedido/src/test/java/com/github/viniciusfcf/ifood/pedido/NativeResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.viniciusfcf.ifood.pedido;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeResourceIT extends ResourceTest {
+
+    // Execute the same tests but in native mode.
+}
